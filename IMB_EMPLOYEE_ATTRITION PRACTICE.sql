56ffd2597af9eb0ec5b3a278f1e0a74891fbdb50
zilphar <116642579+zilphar@users.noreply.github.com>
2024-01-22 21:29:32 +0300

    Update IMB_EMPLOYEE_ATTRITION PRACTICE.sql

--- IMB_EMPLOYEE_ATTRITION PRACTICE.sql
+++ IMB_EMPLOYEE_ATTRITION PRACTICE.sql
@@ -1,27 +1,27 @@
 -- working with the ibm_employee_attrition dataset 
 
--- 1. determining the total number of employees(rows) in the table (returns 412)
+-- determining the total number of employees(rows) in the table (returns 412)
 SELECT COUNT(EmployeeNumber) AS total_no_of_employees
 FROM ibm_employee_attrition;
 
--- 2. The number of columns in the table (returns 34)
+-- The number of columns in the table (returns 34)
 SELECT COUNT(*) no_of_columns
 FROM information_schema.columns
 WHERE Table_name = "ibm_employee_attrition";
 
--- 3. the different types of Business travel and How frequent each is used by employees
+-- the different types of Business travel and How frequent each is used by employees
 SELECT DISTINCT BusinessTravel, COUNT(EmployeeNumber) AS no_of_travels
 FROM ibm_employee_attrition
 GROUP BY BusinessTravel
 ORDER BY no_of_travels DESC;
 
--- 4. Different departments and the number of employees in each (the research and development department has the most employees)
+-- Different departments and the number of employees in each (the research and development department has the most employees)
 SELECT DISTINCT Department, COUNT(EmployeeNumber) AS no_of_employees
 FROM ibm_employee_attrition
 GROUP BY Department
 ORDER BY no_of_employees DESC;
 
--- 5. Education field of the employees and number of employees in each
+-- Education field of the employees and number of employees in each
 SELECT DISTINCT EducationField 
 FROM ibm_employee_attrition; 
 
